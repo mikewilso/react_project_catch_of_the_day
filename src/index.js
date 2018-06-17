a18cc2beadc3134c22bc3ec7ff262da55f680292
Michael Wilson <mikewilsooo@gmail.com>
2018-06-17 16:55:08 -0600

    Added Router, NotFound component

--- src/index.js
+++ src/index.js
@@ -5,5 +5,18 @@ import { BrowserRouter, Match, Miss } from 'react-router';
 import './css/style.css';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 
-render(<App/>, document.querySelector('#main'));
+const Root = () => {
+	return(
+		<BrowserRouter>
+			<div>
+				<Match exactly pattern="/" component={StorePicker}/>
+				<Match pattern="/store/:storeid" component={App}/>
+				<Miss component={NotFound}/>
+			</div>
+		</BrowserRouter>
+	)
+}
+
+render(<Root/>, document.querySelector('#main'));
